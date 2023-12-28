344c9c9dcca6e9cdaf8558f26c5e68f585ffd189
Pooja M <42378263+PoojaM22@users.noreply.github.com>
2023-12-28 23:36:27 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull yasminjeelani/mypythonapp
+docker pull pooja942/mypythonapp
 
 # Run the Docker image as a container
-docker run -d --name mypythoncontainer1 -p 5001:5000 yasminjeelani/mypythonapp
+docker run -d --name mypythoncontainer1 -p 5001:5000 pooja942/mypythonapp
